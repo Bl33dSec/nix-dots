d80c08e6578f87226eaec6492f7c277cae4fb964
bl33dsec <vdlbsec@gmail.com>
2026-02-08 00:21:14 +0100

    Added some settings to firefox

--- users/ben/programs/firefox.nix
+++ users/ben/programs/firefox.nix
@@ -16,6 +16,53 @@
       settings = {
         "widget.gtk.libadwaita-colors.enabled" = false;
       };
+
+      preferences = {
+        "privacy.resistFingerprinting"  = true;
+      };
+
+      policies = {
+        DisableFirefoxAccounts = true;
+        DisableTelemetry = true;
+        HardwareAcceleration = false;
+	OfferToSaveLogins = false;
+      };
+    };
+
+    profiles.default.search = {
+      force = true;
+      default = "DuckDuckGo";
+      privateDefault = "DuckDuckGo";
+
+      engines = {
+        "Nix Packages" = {
+          urls = [
+            {
+              template = "https://search.nixos.org/packages";
+              params = [
+                { name = "channel"; value = "25.11"; }
+                { name = "query";   value = "{searchTerms}"; }
+              ];
+            }
+          ];
+
+          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+          definedAliases = [ "@np" ];
+        };
+
+        "NixOS Wiki" = {
+          urls = [
+            {
+              template = "https://wiki.nixos.org/w/index.php";
+              params = [ { name = "search"; value = "{searchTerms}"; } ];
+            }
+          ];
+
+          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+          definedAliases = [ "@nw" ];
+        };
+      };
     };
   };
 }
+
