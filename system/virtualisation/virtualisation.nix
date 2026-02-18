b1e823c4e1c1ee80e649306651a4aa24ef4229f3
bl33dsec <vdlbsec@gmail.com>
2026-02-18 23:38:16 +0100

    Set GTK to dark

--- system/virtualisation/virtualisation.nix
+++ system/virtualisation/virtualisation.nix
@@ -1,12 +1,24 @@
 { ... }:
 
 {
-  virtualisation = {
-    vmware = {
-      host = {
-        enable = true;
-      };
+  programs = {
+    virt-manager = {
+      enable = true;
     };
   };
-}
 
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+      };
+    };
+
+    spiceUSBRedirection = {
+      enable = true;
+    };
+  };
+
+  users.groups.libvirtd.members = ["ben"];
+}
--- users/ben/home.nix
+++ users/ben/home.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./fonts.nix
+    ./theme.nix
     ./programs
     ./shell
   ];
